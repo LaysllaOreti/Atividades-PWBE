7116fbeb95c3a4989e4ded0668f24840eb25f65e
AkiraSunsets <ketlynlorraynendearaujo@gmail.com>
2025-08-21 16:29:23 -0300

    Aula do dia 21.08

--- Project_Livraria/api/urls.py
+++ Project_Livraria/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import *
- 
-#Crio os endpoints
+
+# Crio os endpoints
 urlpatterns = [
-    path('autores', AutoresView.as_view()) ,#URLS para CRUD no banco de dados
-    path('authors', visualizacao_autor ),
-    path('editoras', EditoraView.as_view()),
-    path('livro', LivroView.as_view())
+    path('autores/', AutoresView.as_view()),   # CRUD autores
+    path('authors/', visualizacao_autor),      # rota extra
+    path('editoras/', EditoraView.as_view()),
+    path('livros/', LivroView.as_view()),
+
+    path('autor/<int:pk>/', AutoresCrud.as_view()),
+    path('editora/<int:pk>/', EditoraCrud.as_view()),
+    path('livro/<int:pk>/', LivroCrud.as_view()),
 ]
- 
