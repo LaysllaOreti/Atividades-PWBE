be18ea31b38b5120b187e658f99a62416e2fe45a
LaysllaOreti <laysllaoreti2006@gmail.com>
2025-09-29 09:37:38 -0300

    Finalização da atividade

--- front/src/app/models/editora.ts
+++
@@ -1,9 +0,0 @@
-export interface Editora {
-    id: number;
-    editora: string;
-    cnpj?: string;
-    endereco?: string;
-    telefone?: string;
-    email?: string;
-    site?: string;
-}
--- front/src/enviroments/enviroments.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-    production: false,
-    apiBase: 'http://127.0.0.1:8000/'
-}
